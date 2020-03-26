9070a8bcb317014b397b969a45a5a812f4634f20
Arthur Tondo <AaronVicius@gmail.com>
2020-03-26 09:37:20 -0300

    adicionando interfasce ex1 aula 3

---
+++ app/src/main/java/com/example/correoex1aula3/Aula31Activity.java
@@ -0,0 +1,13 @@
+package com.example.correoex1aula3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Aula31Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInsanceState){
+        super.onCreate(savedInsanceState);
+        setContentView(R.layout.aula31);
+    }
+}
--- app/src/main/java/com/example/correoex1aula3/MainActivity.java
+++ app/src/main/java/com/example/correoex1aula3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.correoex1aula3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void aula31Click(View view){
+
+        }
+    public void aula32Click(View view){
+
+    }
+    public void aula33Click(View view){
+
+    }
+    public void aula34Click(View view){
+
     }
 }
